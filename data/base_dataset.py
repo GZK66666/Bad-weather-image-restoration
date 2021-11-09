364cc6f75837aff2df3dc3bbdef6aa7a80684c22
guozhaokang <572223078@qq.com>
2021-11-09 16:17:09 +0800

    修复测试时图片的裁剪

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -12,18 +12,40 @@ class BaseDataset(data.Dataset):
     def initialize(self, opt):
         pass
 
-def get_transform(opt):
+# def get_transform(opt):
+#     transform_list = []
+#     if 'resize' in opt.resize_or_crop:
+#         transform_list.append(transforms.Resize(opt.load_size, Image.BICUBIC))
+#
+#     if opt.isTrain:
+#         if 'crop' in opt.resize_or_crop:
+#             transform_list.append(transforms.RandomCrop(opt.crop_size))
+#         if not opt.no_flip:
+#             transform_list.append(transforms.RandomHorizontalFlip())
+#
+#     transform_list += [transforms.ToTensor(),
+#                        transforms.Normalize((0.5, 0.5, 0.5),
+#                                             (0.5, 0.5, 0.5))]
+#     return transforms.Compose(transform_list)
+
+def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
     transform_list = []
+    if grayscale:
+        transform_list.append(transforms.Grayscale(1))
     if 'resize' in opt.resize_or_crop:
-        transform_list.append(transforms.Resize(opt.load_size, Image.BICUBIC))
+        osize = [opt.load_size, opt.load_size]
+        transform_list.append(transforms.Resize(osize, method))
 
-    if opt.isTrain:
-        if 'crop' in opt.resize_or_crop:
-            transform_list.append(transforms.RandomCrop(opt.crop_size))
-        if not opt.no_flip:
-            transform_list.append(transforms.RandomHorizontalFlip())
+    if 'crop' in opt.resize_or_crop:
+        transform_list.append(transforms.RandomCrop(opt.crop_size))
 
-    transform_list += [transforms.ToTensor(),
-                       transforms.Normalize((0.5, 0.5, 0.5),
-                                            (0.5, 0.5, 0.5))]
+    if not opt.no_flip:
+        transform_list.append(transforms.RandomHorizontalFlip())
+
+    if convert:
+        transform_list += [transforms.ToTensor()]
+        if grayscale:
+            transform_list += [transforms.Normalize((0.5,), (0.5,))]
+        else:
+            transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     return transforms.Compose(transform_list)
